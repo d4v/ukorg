06330679b920b2742f23ce3bd6dc4a109149983e
David Bittoun <david.bittoun@gmail.com>
2013-10-09 00:28:36 +0200

    Reindented + added gitignore + removed traces

--- taptempo.cpp
+++ taptempo.cpp
@@ -15,15 +15,12 @@ static const long default_period_ns = 41666667; /* 60bpm, 24 ticks/beat => 1/24 
 volatile long period_ns = default_period_ns;
 
 void tick() {
-	static const char clockTick = 0xf8;
-	ssize_t nb = 0;
+  static const char clockTick = 0xf8;
+  ssize_t nb = 0;
 
   if ((nb = write(fd, &clockTick, 1)) == -1) {
     perror("Failed to write");
   }
-
-	printf(".");
-  fflush(stdout);
 }
 
 #define ONE_SECOND 1000000000 /* 1 second => 1e9 nanosec */
@@ -33,62 +30,56 @@ long getPeriod(const struct timespec *ta, const struct timespec *tb) {
   long delta =   tb->tv_nsec - ta->tv_nsec
                + (tb->tv_sec - ta->tv_sec) * ONE_SECOND;
 
-  printf("period : %ld", delta);
   return delta / TICKSBYBEAT;
 }
 
 void *getTap(void*) {
 
-	// set stdin unbuffered
-	struct termios term,old;
-	tcgetattr(STDIN_FILENO, &old);
-	term = old;
-	term.c_lflag &= ~ICANON;
-	tcsetattr(STDIN_FILENO, TCSANOW, &term);
-	struct timespec ta,tb;
+  // set stdin unbuffered
+  struct termios term,old;
+  tcgetattr(STDIN_FILENO, &old);
+  term = old;
+  term.c_lflag &= ~ICANON & ~ECHO;
+  tcsetattr(STDIN_FILENO, TCSANOW, &term);
+  struct timespec ta,tb;
 
-	clock_gettime(CLOCK_REALTIME,&ta);
+  clock_gettime(CLOCK_REALTIME,&ta);
 
-	while (true) {
-		char c = getchar();
-		if(c == 'q') {
-			break;
-		};
+  while (true) {
+    char c = getchar();
+    if(c == 'q') {
+      break;
+    };
 
+    clock_gettime(CLOCK_REALTIME,&tb);
 
-		clock_gettime(CLOCK_REALTIME,&tb);
-
-		pthread_mutex_lock(&mutex);
+    pthread_mutex_lock(&mutex);
     period_ns = getPeriod(&ta,&tb);
-		bTap = true;
-    perror("signal");
-		pthread_cond_signal(&cond);
-		pthread_mutex_unlock(&mutex);
-	
+    bTap = true;
+    pthread_cond_signal(&cond);
+    pthread_mutex_unlock(&mutex);
 
     ta = tb;
-};
+  };
 
-	tcsetattr(STDIN_FILENO, TCSANOW, &old);
-	return NULL;
+  tcsetattr(STDIN_FILENO, TCSANOW, &old);
+  return NULL;
 }
 
 int main(void) {
 
-	fd = open("/dev/midi1", O_WRONLY);
-	pthread_t thread;
-	struct timespec ts;
+  fd = open("/dev/midi1", O_WRONLY);
+  pthread_t thread;
+  struct timespec ts;
 
-	pthread_create(&thread,NULL,&getTap,NULL);
+  pthread_create(&thread,NULL,&getTap,NULL);
 
-	while (true) {
-		int res = 0;
-		pthread_mutex_lock(&mutex);
+  while (true) {
+    int res = 0;
+    pthread_mutex_lock(&mutex);
 
-		while(!bTap) {
+    while(!bTap) {
       clock_gettime(CLOCK_REALTIME,&ts);
-
-//      period_ns = ONE_SECOND;
 
       ts.tv_sec  += period_ns / ONE_SECOND;
       ts.tv_nsec += period_ns % ONE_SECOND;
@@ -97,13 +88,13 @@ int main(void) {
         ts.tv_nsec -= ONE_SECOND;
       }
 
-			res = pthread_cond_timedwait(&cond,&mutex,&ts);
-			if(res == ETIMEDOUT) break;
-		}
-		tick();
-		bTap = false;
-		pthread_mutex_unlock(&mutex);
-	}
+      res = pthread_cond_timedwait(&cond,&mutex,&ts);
+      if(res == ETIMEDOUT) break;
+    }
+    tick();
+    bTap = false;
+    pthread_mutex_unlock(&mutex);
+  }
 
-	return 0;
+  return 0;
 }
