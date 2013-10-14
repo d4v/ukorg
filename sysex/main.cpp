5279a5f1fc411146265123c739ee4bb399136172
David Bittoun <david.bittoun@gmail.com>
2013-10-14 23:25:57 +0200

    Data to MidiData converter works

--- sysex/main.cpp
+++ sysex/main.cpp
@@ -1,5 +1,28 @@
 #include <fcntl.h>
+#include <string.h>
 #include <iostream>
+
+/**
+ * MIDI data converter
+ *
+ * Explanations from "micro KORG MIDI Implementation" Revision 1.4 (2002.06.27)
+ *
+ *
+ * DATA ( 1set = 8bit x 7Byte )
+ * b7      ~     b0   b7      ~     b0   b7     ~~      b0   b7     ~      b0
+ * +-+-+-+-+-+-+-+-+  +-+-+-+-+-+-+-+-+  +-+-+-+-~~-+-+-+-+  +-+-+-+-+-+-+-+-+
+ * | | | | | | | | |  | | | | | | | | |  | | | |    | | | |  | | | | | | | | |
+ * +-+-+-+-+-+-+-+-+  +-+-+-+-+-+-+-+-+  +-+-+-+-~~-+-+-+-+  +-+-+-+-+-+-+-+-+
+ *       7n+0               7n+1          7n+2   ~~   7n+5        7n+6
+ *
+ * MIDI DATA ( 1set = 7bit x 8Byte )
+ *    b7b7b7b7b7b7b7     b6    ~     b0     b6   ~~~     b0     b6    ~     b0
+ * +-+-+-+-+-+-+-+-+  +-+-+-+-+-+-+-+-+  +-+-+-+-~~-+-+-+-+  +-+-+-+-+-+-+-+-+
+ * |0| | | | | | | |  |0| | | | | | | |  |0| | |    | | | |  |0| | | | | | | |
+ * +-+-+-+-+-+-+-+-+  +-+-+-+-+-+-+-+-+  +-+-+-+-~~-+-+-+-+  +-+-+-+-+-+-+-+-+
+ * 7n+6,5,4,3,2,1,0         7n+0          7n+1  ~~~  7n+5          7n+6
+ *
+ */
 
 using namespace std;
 
@@ -21,16 +44,39 @@ string bitdump(const char c) {
   return s;
 }
 
-void toMidi(unsigned char * buf_out, unsigned const char * buf_in, size_t size) {
+void toMidi(unsigned char * buf_out,
+            unsigned const char * buf_in, size_t size) {
+  /* Initilize out buffer */
   memset(buf_out,0,8);
-  unsigned char *firstByte = buf_out[0];
-  for(int idx = 0; idx < 7; idx++) {
 
+  /* First byte gathers the 7 following bytes first bit*/
+  unsigned char *firstByte = buf_out;
+  *firstByte = 0x00;
 
+  /* First bit mask */
+  static const char firstBit = 0x80; /* 1000 0000 */
+
+  /* Byte to byte treatment */
+  int max = size < 7 ? size : 7;
+  for(int idx = 0; idx < max; idx++) {
+    /* Get current byte first bit */
+    unsigned char tmpByte = buf_in[idx] & firstBit;
+    if(tmpByte) {
+      /* First bit is 1 */
+      tmpByte >>= idx+1;
+      /* Set first byte corresponding bit */
+      *firstByte |= tmpByte;
+      /* Set the 6 others bits in the out chunk */
+      buf_out[idx+1] = buf_in[idx] & ~firstBit;
+    } else {
+      /* First bit is 0 */
+      /* Just put the byte at its place in out_buf */
+      buf_out[idx+1] = buf_in[idx];
+    }
   }
 }
 
-void dumpChunk(const char * chunk, size_t nb) {
+void dumpChunk(const unsigned char * chunk, size_t nb) {
   for(int idx = 0; idx < nb; idx++) {
     cout << bitdump(chunk[idx]) << "  ";
   }
@@ -47,12 +93,14 @@ int main(int argc, char* argv[]) {
   static const size_t chunkSize = 7;
 
   int nbRead = 0;
-  char buf_in[chunkSize];
+  unsigned char buf_in[chunkSize];
+  memset(buf_in,0,chunkSize);
   while((nbRead = read(data_in,buf_in,chunkSize)) > 0) {
-    char buf_out[8];
+    unsigned char buf_out[8];
     toMidi(buf_out,buf_in,nbRead);
     dumpChunk(buf_in,chunkSize);
     dumpChunk(buf_out,8);
+    memset(buf_in,0,chunkSize);
   }
 
   return 0;
